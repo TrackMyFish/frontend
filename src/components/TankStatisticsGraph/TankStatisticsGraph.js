d34303ca4c849f04f868205e3cf713963c449864
Simon Drake <simondrake1990@gmail.com>
2021-08-27 18:04:20 +0100

    something

--- src/components/TankStatisticsGraph/TankStatisticsGraph.js
+++ src/components/TankStatisticsGraph/TankStatisticsGraph.js
@@ -13,10 +13,69 @@ import {
 
 import "react-table-6/react-table.css";
 
+const getBiggest = (acc, cv) => {
+  if (!cv) {
+    return acc;
+  }
+
+  if (!acc) {
+    return cv;
+  }
+
+  if (cv > acc) {
+    return cv;
+  }
+
+  return acc;
+};
+
 const TankStatisticsGraph = observer((props) => {
   const sorted = props.tankStatistics
     .filter((e) => e.testDate !== "")
-    .sort((a, b) => new Date(a.testDate) - new Date(b.testDate));
+    .sort((a, b) => new Date(a.testDate) - new Date(b.testDate))
+    .reduce((acc, cv) => {
+      for (let i = 0; i < acc.length; i++) {
+        if (acc[i].testDate === cv.testDate) {
+          console.log(cv.ph, acc[i].ph);
+
+          let obj = {
+            testDate: cv.testDate,
+            ammonia: cv.ammonia
+              ? cv.ammonia > acc[i].ammonia
+                ? cv.ammonia
+                : acc[i].ammonia
+              : acc[i].ammonia,
+            // ph: cv.ph ? (cv.ph > acc[i].ph ? cv.ph : acc[i].ph) : acc[i].ph,
+            ph: getBiggest(cv.ph, acc[i].ph),
+            nitrite: cv.nitrite
+              ? cv.nitrite > acc[i].nitrite
+                ? cv.nitrite
+                : acc[i].nitrite
+              : acc[i].nitrite,
+            nitrate: cv.nitrate
+              ? cv.nitrate > acc[i].nitrate
+                ? cv.nitrate
+                : acc[i].nitrate
+              : acc[i].nitrate,
+            gh: cv.gh ? (cv.gh > acc[i].gh ? cv.gh : acc[i].gh) : acc[i].gh,
+            kh: cv.kh ? (cv.kh > acc[i].kh ? cv.kh : acc[i].kh) : acc[i].kh,
+            phosphate: cv.phosphate
+              ? cv.phosphate > acc[i].phosphate
+                ? cv.phosphate
+                : acc[i].phosphate
+              : acc[i].phosphate,
+          };
+
+          acc[i] = obj;
+          return acc;
+        }
+      }
+
+      acc.push(cv);
+      return acc;
+    }, []);
+
+  console.log(sorted);
 
   return (
     <ResponsiveContainer width="100%" height="50%">
